aa3ece57ab6cc67d821a249cfd2d55c108d3e8d4
xuejinrui <1091448008@qq.com>
2022-11-01 13:21:06 +0800

    first commit

---
+++ src/main/java/com/xjr/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xjr.git;
+
+/**
+ * @author XJR
+ * @date 2022/11/1 13:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
